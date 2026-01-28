bd4ff269b7a06300e509d8505d4470114c6b9345
jairamkarthik <100018061+jairamkarthik@users.noreply.github.com>
2026-01-29 00:15:17 +0530

    added project files

---
+++ src/main/java/com/docker_deploy/App.java
@@ -0,0 +1,7 @@
+package com.docker_deploy;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Java Maven App running inside Docker via Jenkins");
+    }
+}
---
+++ src/test/java/com/docker_deploy/AppTest.java
@@ -0,0 +1,12 @@
+package com.docker_deploy;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class AppTest {
+
+    @Test
+    void testApp() {
+        assertTrue(true);
+    }
+}
